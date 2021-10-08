de53e4a6e6648a2aacc6fc3af5f5420ca7c2cdf6
Chaim <chaim.straus@gmail.com>
2021-10-07 20:22:13 -0400

    renaming, also adds unedited codes that I was given to work with for the assignments

---
+++ unedited codes/1. Input Output User Log/1. Input Output User Log/TestCode/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace TestCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter username:");
+
+            string userName = Console.ReadLine();
+
+            Console.WriteLine("Username is: " + userName);
+
+            string recordValue = "We recorded the username " + userName;
+
+            using (StreamWriter sw = File.AppendText("holder.txt"))
+            {
+                sw.WriteLine(recordValue);
+            }
+            string readText = File.ReadAllText("holder.txt"); // Read the contents of the file
+            Console.WriteLine(readText); // Output the content
+        }
+    }
+}
---
+++ unedited codes/2. File Hierarchy FIRST PASS/2. File Hierarchy/2. File Hierarchy/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace TestCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int this_year = 2022;
+
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            
+            Console.WriteLine("What year were you born?");
+            int birth_year = Console.ReadLine();
+
+            Console.WriteLine(name + " was born in " + birth_year);
+            Console.WriteLine("That was " + " years ago.");
+
+        // Ignore this for now
+        // save the info in two separate files
+        // place those files into one master folder
+        // Now output.... "XX was born in XX.  That was XX years ago."
+        }
+    }
+}
