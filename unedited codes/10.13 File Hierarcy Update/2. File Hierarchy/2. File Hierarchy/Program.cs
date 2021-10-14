d494bc7d82da05c9771fc169c721ee09a9974057
Chaim <chaim.straus@gmail.com>
2021-10-14 16:27:11 -0400

    class material Oct. 14

--- unedited codes/10.13 File Hierarcy Update/2. File Hierarchy/2. File Hierarchy/Program.cs
+++ unedited codes/10.13 File Hierarcy Update/2. File Hierarchy/2. File Hierarchy/Program.cs
@@ -7,17 +7,17 @@ namespace TestCode
     {
         static void Main(string[] args)
         {
-            int this_year = Convert.ToInt32(DateTime.UtcNow.ToString("yyyy"));
+            int current_year = DateTime.Now.Year;
 
             Console.WriteLine("What is your name?");
             string name = Console.ReadLine();
 
             Console.WriteLine("What year were you born?");
-            int birth_year = Convert.ToInt32(  Console.ReadLine() );
+            int birth_year = Convert.ToInt32(Console.ReadLine() );
 
             Console.Write($"{name} was born in {birth_year}");
-            Console.WriteLine(" of the Common Era.");
-            Console.WriteLine("That was " + (this_year - birth_year) + " years ago.");
+            Console.Write(" of the Common Era.");
+            Console.WriteLine("That was " + (current_year - birth_year) + " years ago.");
 
             /* TUTORIAL:
              * Type Casting:    https://www.w3schools.com/cs/cs_type_casting.php
@@ -32,6 +32,15 @@ namespace TestCode
              * Inside holder, save the name and date info in .txt files called name.txt and date.txt
              * When the program runs, it will output ALL of the names, dates, and ages on record.
             */
+
+            using (StreamWriter names = File.AppendText("data\\date.txt"))
+            using (StreamWriter birth_years = File.AppendText("data\\name.txt"))
+            using (StreamWriter output = File.AppendText("data\\output.txt"))
+            {
+                names.WriteLine(name);
+                birth_years.WriteLine(birth_year);
+                output.WriteLine($"{name} was born in {birth_year} of the Common Era. That was {current_year - birth_year} years ago.");
+            }
         }
     }
 }
